bb23f0428acba78029213d9a63bd3655ca13c386
Rizwan <rizwanshaikh2200@gmail.com>
2024-08-18 17:50:58 -0400

    hi

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { Container, Box, Typography, AppBar, Toolbar, Button } from '@mui/material'
-import { SignIn } from '@clerk/nextjs'
-import Link from 'next/link'
+import {
+    AppBar,
+    Box,
+    Button,
+    Container,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import Link from "next/link";
+import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
     return (
