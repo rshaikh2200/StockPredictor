4c192d21e314c09f23be383a0b4c1d1e7d1e12fb
Rizwan <rizwanshaikh2200@gmail.com>
2024-08-18 18:04:21 -0400

    hi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY
+});
 
 export const config = {
   matcher: [
