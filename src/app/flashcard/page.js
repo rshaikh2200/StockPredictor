f48078786a160f76fd907b4389e954b3cfa71b36
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-22 00:38:04 +0000

    hi

--- src/app/flashcard/page.js
+++ src/app/flashcard/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { db } from "../firebase.js";
+import { db } from "../firebase";
 import { useUser } from "@clerk/nextjs";
 import { Box, Container, Grid, Paper, Typography } from "@mui/material";
 import { collection, doc, getDocs } from "firebase/firestore";
