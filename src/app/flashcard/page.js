0259d7c0af59cdfe73e4d17fb195640de599ff0c
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-22 01:17:43 +0000

    hello

--- src/app/flashcard/page.js
+++ src/app/flashcard/page.js
@@ -10,10 +10,13 @@ import { useEffect, useState } from "react";
 import Appbar from "/src/app/components/Appbar";
 import FlipCard from "/src/app/components/FlipCard";
 
-const Page = () => {
+export default function Flashcard() {
     const router = useRouter();
     const { isLoaded, isSignedIn, user } = useUser();
     const [flashcards, setFlashcards] = useState([]);
+  
+    const searchParams = useSearchParams();
+    const search = searchParams.get("id");
   
     useEffect(() => {
       if (isLoaded) {
@@ -24,70 +27,50 @@ const Page = () => {
     }, [isLoaded, router, user]);
   
     useEffect(() => {
-      async function getFlashcards() {
-        if (!user) return;
-        const docRef = doc(collection(db, "users"), user.id);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          const flashcards = docSnap.data().flashcards || [];
-          setFlashcards(flashcards);
-        } else {
-          await setDoc(docRef, { flashcards: [] });
-        }
+      async function getFlashcard() {
+        if (!search || !user) return;
+  
+        const colRef = collection(doc(collection(db, "users"), user.id), search);
+        const docs = await getDocs(colRef);
+        const flashcards = [];
+        docs.forEach((doc) => {
+          flashcards.push({ id: doc.id, ...doc.data() });
+        });
+        setFlashcards(flashcards);
       }
-      getFlashcards();
-    }, [user]);
+      getFlashcard();
+    }, [search, user]);
   
-    const handleClick = (id) => {
-      router.push(`/flashcard?id=${id}`);
-    };
+    if (!isLoaded && !isSignedIn) return <Loader />;
   
-    if (!isLoaded) return null; // Removed Loader and adjusted condition
     return (
       <Box>
         <Appbar />
         <Container maxWidth="md">
-          <Typography variant="h2" sx={{ my: 4 }}>
-            My Flashcards
+        <Typography variant="h2" sx={{ my: 4 }}>
+            {search}
           </Typography>
-          {flashcards.length === 0 ? (
-            <Paper
-              sx={{
-                p: 4,
-                align: "center",
-                backgroundColor: (theme) => theme.palette.tangaroa[200],
-              }}
-            >
-              <Typography variant="p" component="div">
-                You don&apos;t have any flashcards yet. <br /> To create one, go
-                to the <Link href="/generate">Create Flashcard</Link> page.
-              </Typography>
-            </Paper>
-          ) : (
-            <Grid
-              container
-              columnSpacing={2}
-              rowSpacing={1}
-              sx={{ mt: 4, minHeight: "12rem" }}
-            >
-              {flashcards.map((flashcard, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                  <Card>
-                    <CardActionArea onClick={() => handleClick(flashcard.name)}>
-                      <CardContent>
-                        <Typography variant="h5" component="div">
-                          {flashcard.name}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          )}
+        {flashcards.length === 0 && (
+          <Paper
+           
+          >
+            <Typography variant="p" component="div">
+              You don&apos;t have any flashcards of {search}. <br /> To create
+              one, go to the <Link href="/generate">Create Flashcard</Link> page.{" "}
+              <br /> To view the flashcards, go to the{" "}
+              <Link href="/flashcards">My Flashcards</Link> page.
+            </Typography>
+          </Paper>
+        )}
+          <Grid container spacing={3} my={4}>
+            {flashcards.map((flashcard, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <FlipCard flashcard={flashcard} index={index} />
+              </Grid>
+            ))}
+          </Grid>
         </Container>
+        <Footer />
       </Box>
     );
-  };
-  
-  export default Page;
+  }
--- src/app/flashcards/page.js
+++ src/app/flashcards/page.js
@@ -8,57 +8,88 @@ import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
 import { db } from '../../firebase'
 import Appbar from "../components/Appbar";
 
-export default function Flashcard() {
-  const { isLoaded, isSignedIn, user } = useUser()
-  const [flashcards, setFlashcards] = useState([])
-  const router = useRouter()
+const Page = () => {
+  const router = useRouter();
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (!user) {
+        router.push("/sign-in");
+      }
+    }
+  }, [isLoaded, router, user]);
 
   useEffect(() => {
     async function getFlashcards() {
-      if (!user) return
-      const docRef = doc(collection(db, 'users'), user.id)
-      const docSnap = await getDoc(docRef)
+      if (!user) return;
+      const docRef = doc(collection(db, "users"), user.id);
+      const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        const collections = docSnap.data().flashcards || []
-        setFlashcards(collections)
+        const flashcards = docSnap.data().flashcards || [];
+        setFlashcards(flashcards);
       } else {
-        await setDoc(docRef, { flashcards: [] })
+        await setDoc(docRef, { flashcards: [] });
       }
     }
-    getFlashcards()
-  }, [user])
+    getFlashcards();
+  }, [user]);
 
+  const handleClick = (id) => {
+    router.push(`/flashcard?id=${id}`);
+  };
+
+  if (!isLoaded && !isSignedIn) return <Loader></Loader>;
   return (
-    <Container maxWidth="md">
-      <Grid container spacing={3} sx={{ mt: 4 }}>
-        {flashcards.map((flashcard) => (
-          <Grid item xs={12} sm={6} md={4} key={flashcard.id}>
-            <Card>
-              <CardActionArea onClick={() => handleCardClick(flashcard.id)}>
-                <CardContent>
-                  <Box sx={{ /* Styling for flip animation */ }}>
-                    <div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.front}
-                        </Typography>
-                      </div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.back}
-                        </Typography>
-                      </div>
-                    </div>
-                  </Box>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+    <Box>
+      <Appbar />
+      <Container maxWidth="md">
+        <Typography variant="h2" sx={{ my: 4 }}>
+          My Flashcards
+        </Typography>
+        {flashcards.length === 0 ? (
+          <Paper
+            sx={{
+              p: 4,
+              align: "center",
+              backgroundColor: (theme) => theme.palette.tangaroa[200],
+            }}
+          >
+            <Typography variant="p" component="div">
+              You don&apos;t have any flashcards yet. <br /> To create one, go
+              to the <Link href="/generate">Create Flashcard</Link> page.
+            </Typography>
+          </Paper>
+        ) : (
+          <Grid
+            container
+            columnSpacing={2}
+            rowSpacing={1}
+            sx={{ mt: 4, minHeight: "12rem" }}
+          >
+            {flashcards.map((flashcard, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card>
+                  <CardActionArea onClick={() => handleClick(flashcard.name)}>
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {flashcard.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Container>
-  )  
-  const handleCardClick = (id) => {
-    router.push(`/flashcard?id=${id}`)
-  }
-}
+        )}
+      </Container>
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </Box>
+  );
+};
+
+export default Page;
